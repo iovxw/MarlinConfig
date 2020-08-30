9e5a80b5ee86af999b48446e22df886dcb146539
iovxw <iovxw@outlook.com>
2020-08-30 18:17:39 +0800

    BMG extruder

--- config/default/Configuration.h
+++ config/default/Configuration.h
@@ -747,7 +747,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 382.165 }
 
 /**
  * Default Max Feed Rate (mm/s)
